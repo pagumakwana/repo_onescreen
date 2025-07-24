c1fa847af7166aaad28aa3bc00d8661598220538
Pragnesh Makwana <info@webdroids.in>
2025-07-24 11:01:46 +0530

    Update new changes

--- app_onespace/src/app/home-module/home-module.component.ts
+++ app_onespace/src/app/home-module/home-module.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
 @Component({
   selector: 'app-home-module',
   standalone: true,
   imports: [],
   templateUrl: './home-module.component.html',
-  styleUrl: './home-module.component.scss'
+  styleUrl: './home-module.component.scss',
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomeModuleComponent {
 
