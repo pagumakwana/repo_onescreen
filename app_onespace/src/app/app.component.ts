20d564fa6e7a76174b781ba5e1b5a1cb83da9f40
Pragnesh Makwana <info@webdroids.in>
2025-07-24 15:54:39 +0530

    update

--- app_onespace/src/app/app.component.ts
+++ app_onespace/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, Renderer2 } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,4 +10,20 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'app_onespace';
+  private router = inject(Router);
+  private renderer = inject(Renderer2);
+
+  constructor() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event:any) => {
+        const body = document.body;
+
+        if (event.url.includes('/app')) {
+          this.renderer.removeAttribute(body, 'data-pc-layout');
+        } else {
+          this.renderer.setAttribute(body, 'data-pc-layout', 'horizontal');
+        }
+      });
+  }
 }
