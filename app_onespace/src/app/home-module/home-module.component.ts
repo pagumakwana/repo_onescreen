3b4653a13ff19af2c48607056e4cafb35d4bd24d
Pallavi Chapla <52373477+PallaviChapla@users.noreply.github.com>
2025-07-27 16:56:21 +0530

    css added

--- app_onespace/src/app/home-module/home-module.component.ts
+++ app_onespace/src/app/home-module/home-module.component.ts
@@ -1,4 +1,4 @@
-import { Component,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component,CUSTOM_ELEMENTS_SCHEMA, ViewEncapsulation } from '@angular/core';
 // import function to register Swiper custom elements
 import { register } from 'swiper/element/bundle';
 // register Swiper custom elements
@@ -9,7 +9,8 @@ register();
   imports: [],
   templateUrl: './home-module.component.html',
   styleUrl: './home-module.component.scss',
-  schemas:[CUSTOM_ELEMENTS_SCHEMA]
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
+encapsulation: ViewEncapsulation.None
 })
 export class HomeModuleComponent {
 
