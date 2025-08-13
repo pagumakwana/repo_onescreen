5eb542d84466cb1847e6234dca0b4aff214d9db5
Pragnesh Makwana <info@webdroids.in>
2025-08-14 01:55:13 +0530

    update new changes

--- app_onespace/src/app/adminmodeules/layoutmaster/admin-sidebar/admin-sidebar.component.ts
+++ app_onespace/src/app/adminmodeules/layoutmaster/admin-sidebar/admin-sidebar.component.ts
@@ -1,13 +1,109 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import _ from 'lodash';
+import { enAppSession } from '../../../_appmodel/sessionstorage';
+import { menuStructure } from '../../../_appmodel/_model';
+import { BaseServiceHelper } from '../../../_appservice/baseHelper.service';
 
 @Component({
   selector: 'app-admin-sidebar',
   standalone: true,
-  imports:  [RouterModule],
+  imports: [RouterModule,CommonModule],
   templateUrl: './admin-sidebar.component.html',
   styleUrl: './admin-sidebar.component.scss'
 })
-export class AdminSidebarComponent {
+export class AdminSidebarComponent implements OnInit {
 
+  constructor(public _base: BaseServiceHelper, private _cdr: ChangeDetectorRef) { }
+
+  userModule: any;
+  menuStructure: Array<any> = []
+  _menuStructure: menuStructure = {};
+  public fullname: string = '';
+  @Output() allMenu: EventEmitter<any> = new EventEmitter();
+
+  ngOnInit(): void {
+    this.getAuthorityModule();
+  }
+
+  getAuthorityModule() {
+    this._base._encryptedStorage.get(enAppSession.user_id).then(user_id => {
+      this._base._commonService.getauthoritymodule(user_id).then((resUserModule: any) => {
+        debugger
+        this.userModule = [];
+        this.userModule = Array.isArray(resUserModule) ? resUserModule : [];
+        this.menuStructure = this.list_to_tree(this.userModule)
+        console.log(this.menuStructure);
+        this._cdr.detectChanges();
+      });
+    });
+  }
+
+  generateMenuData(arrayData: any) {
+    for (let obj of arrayData) {
+      if (obj.module_parent_id == 0) {
+        this.menuStructure.push(obj)
+      } else {
+
+      }
+    }
+  }
+
+  getChildData(dataId: any) {
+    let index = _.findIndex(this.userModule, (o: any) => {
+      o.module_id = dataId
+    })
+    return index > -1 ? this.userModule[index] : []
+  }
+
+
+  list_to_tree(list: any) {
+    let map: { [key: number]: number } = {}, node, roots = [], i;
+
+    for (i = 0; i < list.length; i += 1) {
+      map[list[i].module_id] = i; // initialize the map
+      list[i].children = []; // initialize the children
+    }
+
+    for (i = 0; i < list.length; i += 1) {
+      node = list[i];
+      if (node.module_parent_id !== parseInt("0")) {
+        // if you have dangling branches check that map[node.module_parent_id] exists
+        list[map[node.module_parent_id]]?.children?.push(node);
+      } else {
+        roots.push(node);
+      }
+    }
+    return roots;
+  }
+
+  logout() {
+    this._base._appSessionService.clearUserSession();
+    setTimeout(() => {
+      this._base._router.navigate(['/auth']);
+    }, 1000);
+  }
+
+  onRouterLinkActive(event: any) {
+    console.log("event", event);
+  }
+
+  navigateToMenu(menu: any, allMenu: any) {
+    this._menuStructure = {
+      module_id: menu.module_id,
+      module_parent_id: menu.module_parent_id,
+      moduleicon: menu.moduleicon,
+      modulerouting: menu.modulerouting,
+      children: menu.children,
+      meuItems: allMenu,
+      modulename: menu.modulename
+    }
+    this.allMenu.emit(this._menuStructure);
+    if (menu.modulerouting != '' && menu.modulerouting != undefined && menu.modulerouting != null) {
+      this._base._router.navigate([menu.modulerouting]);
+    } else {
+      this._base._router.navigate(['app', menu.modulename]);
+    }
+  }
 }
--- app_onespace/src/app/authmodule/authmodule.component.ts
+++ app_onespace/src/app/authmodule/authmodule.component.ts
@@ -1,12 +1,141 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { Validators } from 'ngx-editor';
+import { Observable, Subscription, first } from 'rxjs';
+import { userModel } from '../_appmodel/_model';
+import { BaseServiceHelper } from '../_appservice/baseHelper.service';
+import { WebDService } from '../_appservice/webdpanel.service';
+import { AuthService } from './_authservice/auth.service';
 
 @Component({
   selector: 'app-authmodule',
   standalone: true,
-  imports: [],
+  imports: [RouterModule,FormsModule,ReactiveFormsModule,CommonModule],
   templateUrl: './authmodule.component.html',
-  styleUrl: './authmodule.component.scss'
+  styleUrl: './authmodule.component.scss',
+  providers:[AuthService,WebDService,BaseServiceHelper]
 })
 export class AuthmoduleComponent {
+  hasError: boolean | undefined;
+  isLoading$: Observable<boolean>;
 
+  // private fields
+  private unsubscribe: Subscription[] = []; // Read more: => https://brianflove.com/2016/12/11/anguar-2-unsubscribe-observables/
+
+  constructor(
+    public _base: BaseServiceHelper,
+    private _fbSignIn: FormBuilder,
+    private authService: AuthService,
+    private _webDService: WebDService,
+  ) {
+    this.isLoading$ = this.authService.isLoading$;
+    // redirect to home if already logged in
+    if (this.authService.currentUserValue) {
+      this._base._router.navigate(['/']);
+    }
+  }
+
+  public formSignIn!: FormGroup;
+  _userModel: userModel = {};
+  returnUrl!: string;
+
+  ngOnInit(): void {
+    this.initForm();
+
+  }
+
+  initForm() {
+    this.formSignIn = this._fbSignIn.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  setSignInModel() {
+    debugger
+    this._base._commonService.markFormGroupTouched(this.formSignIn)
+    if (this.formSignIn.valid) {
+      this._userModel.username = this.formSignIn.value.username;
+      this._userModel.password = this.formSignIn.value.password;
+      this.SignInCustomer(this._userModel.username, this._userModel.password);
+    } else {
+      // this.isLoading$ = false;
+    }
+  }
+  SignInCustomer(_username: string = '', _passsword: string = '') {
+    this.hasError = false;
+    const loginSubscr = this.authService
+      .login(_username, _passsword)
+      .pipe(first())
+      .subscribe((user: userModel | undefined) => {
+        if (user) {
+          this.getUserConfig(user.user_id).then(resUserConfig => {
+            this._base._appSessionService.setUserSession(user, (resUserConfig as any[])[0]).subscribe((res: any) => {
+              if (res) {
+                this._base._commonService.getauthoritymodule(user.user_id).then((resUserModule: any) => {
+                  let UserModule: any[] = [];
+                  UserModule = Array.isArray(resUserModule) ? resUserModule : [];
+                  setTimeout(() => {
+                    debugger
+                    this._base._router.navigate([UserModule[0].modulerouting]);
+                  }, 500);
+
+                });
+              }
+            })
+          }, error => {
+            // this.isLoading = false;
+            //this._base._alertMessageService.error("Please try after some time or Contact Support!");
+          });
+        } else {
+          this.hasError = true;
+        }
+      });
+    this.unsubscribe.push(loginSubscr);
+  }
+
+  getUserConfig(user_id: any) {
+    return new Promise((resolve, reject) => {
+      this._webDService.getuserconfig(user_id).subscribe((resUserModule: any) => {
+        let UserModule = [];
+        UserModule = Array.isArray(resUserModule) ? resUserModule : [];
+        resolve(UserModule)
+      }, error => {
+        resolve(false);
+      });
+    });
+  }
+  getUserModule(user_id: any) {
+    return new Promise((resolve, reject) => {
+      this._base._commonService.getauthoritymodule(user_id).then((resUserModule: any) => {
+        let UserModule = [];
+        UserModule = Array.isArray(resUserModule.data) ? resUserModule.data : [];
+        resolve(UserModule)
+      }, error => {
+        resolve(false);
+      });
+    });
+  }
+
+  // submit() {
+  //   this.hasError = false;
+  //   const loginSubscr = this.authService
+  //     .login(this.f.email.value, this.f.password.value)
+  //     .pipe(first())
+  //     .subscribe((user: UserModel | undefined) => {
+  //       debugger
+  //       if (user) {
+  //         this.router.navigate([this.returnUrl]);
+  //       } else {
+  //         this.hasError = true;
+  //       }
+  //     });
+  //   this.unsubscribe.push(loginSubscr);
+  // }
+
+  ngOnDestroy() {
+    this.unsubscribe.forEach((sb) => sb.unsubscribe());
+  }
 }
