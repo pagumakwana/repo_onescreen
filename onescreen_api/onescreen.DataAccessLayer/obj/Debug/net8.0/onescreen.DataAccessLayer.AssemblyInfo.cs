3764cd5bf77cd13922e0676c3104bca5637e54bf
Pragnesh Makwana <info@webdroids.in>
2025-10-29 11:10:12 +0530

    update changes

--- onescreen_api/onescreen.DataAccessLayer/obj/Debug/net8.0/onescreen.DataAccessLayer.AssemblyInfo.cs
+++ onescreen_api/onescreen.DataAccessLayer/obj/Debug/net8.0/onescreen.DataAccessLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("onescreen.DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ac6a4c23a6c05e94d3a095d8a683adfc2e2e80e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2209957c849a5889c95ae98745efef94ab685ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("onescreen.DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("onescreen.DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
