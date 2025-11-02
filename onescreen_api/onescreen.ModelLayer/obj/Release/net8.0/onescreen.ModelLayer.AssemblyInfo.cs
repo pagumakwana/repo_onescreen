42345d18bc4d3f72c6ab046df2aa4a369c0ecc5c
Pragnesh Makwana <info@webdroids.in>
2025-11-02 21:46:13 +0530

    new changes

--- onescreen_api/onescreen.ModelLayer/obj/Release/net8.0/onescreen.ModelLayer.AssemblyInfo.cs
+++ onescreen_api/onescreen.ModelLayer/obj/Release/net8.0/onescreen.ModelLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("onescreen.ModelLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+296baec113a778d1585d56342452a23b0acf6317")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a079a159646405ce806f2a0391be1261e146f1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("onescreen.ModelLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("onescreen.ModelLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
