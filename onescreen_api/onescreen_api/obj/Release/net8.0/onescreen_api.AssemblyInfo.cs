28ef2995fcc035ebee4b44dc13901f3331edf4ad
Pragnesh Makwana <info@webdroids.in>
2026-03-09 13:20:50 +0530

    update changes

--- onescreen_api/onescreen_api/obj/Release/net8.0/onescreen_api.AssemblyInfo.cs
+++ onescreen_api/onescreen_api/obj/Release/net8.0/onescreen_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("onescreen_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2b405330f786123e6c970f0f415939ab8cac2fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c90ab49cd3f8d4b8d0abe399eebdda7078bf8ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("onescreen_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("onescreen_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
